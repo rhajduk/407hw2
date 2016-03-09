3e6d03adb0ac8e1cc1525809c6f4f21438284134
Rebecca Hajduk <rebeccahajduk@rebeccas-macbook-pro-2.local>
2016-03-08 20:42:15 -0600

    Added month label and coded it to change when the view changes months

--- 407hw2/ViewController.swift
+++ 407hw2/ViewController.swift
@@ -13,9 +13,13 @@ class ViewController: UIViewController, CVCalendarViewDelegate, CVCalendarMenuVi
 
     @IBOutlet weak var calendarView: CVCalendarView!
     @IBOutlet weak var menuView: CVCalendarMenuView!
+    //variable for selected date
+    @IBOutlet weak var monthLabel: UILabel!
+    var month: Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       monthLabel.text = formatMonth(calendarView.presentedDate.month)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -38,7 +42,29 @@ class ViewController: UIViewController, CVCalendarViewDelegate, CVCalendarMenuVi
     func firstWeekday() -> Weekday {
         return .Sunday
     }
+    
+    func presentedDateUpdated(date: Date){
+        monthLabel.text = formatMonth(calendarView.presentedDate.month)
+    }
 
+    func formatMonth(month: Int) -> String{
+        var monthString: String = "";
+        if (month == 1){ monthString = "January" }
+        if (month == 2){ monthString = "February" }
+        if (month == 3){ monthString = "March" }
+        if (month == 4){ monthString = "April" }
+        if (month == 5){ monthString = "May" }
+        if (month == 6){ monthString = "June" }
+        if (month == 7){ monthString = "July" }
+        if (month == 8){ monthString = "August" }
+        if (month == 9){ monthString = "September" }
+        if (month == 10){ monthString = "October" }
+        if (month == 11){ monthString = "November" }
+        if (month == 12){ monthString = "December" }
+        
+        
+        return monthString
+    }
 
 }
 

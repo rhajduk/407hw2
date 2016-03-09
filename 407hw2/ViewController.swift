581f84ea261c2be4fae54d65d9863be02fdac26e
Rebecca Hajduk <rebeccahajduk@rebeccas-macbook-pro-2.local>
2016-03-08 19:38:50 -0600

    Added a calendar view and a menu view using the pod

--- 407hw2/ViewController.swift
+++ 407hw2/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import CVCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CVCalendarViewDelegate, CVCalendarMenuViewDelegate{
 
+    @IBOutlet weak var calendarView: CVCalendarView!
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews(){
+        super.viewDidLayoutSubviews()
+        
+        calendarView.commitCalendarViewUpdate()
+        menuView.commitMenuViewUpdate()
+    }
+    
+    func presentationMode() -> CalendarMode{
+        return CalendarMode.MonthView
+    }
+    
+    func firstWeekday() -> Weekday {
+        return .Sunday
     }
 
 
